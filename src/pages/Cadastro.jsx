925873366faf43b0157d2ecd0df2cb98a1e943fa
davyson <davyssonlevi2.0@gmail.com>
2026-05-15 16:34:52 -0300

    ajeitando-para-ajeitar-no-cloud

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -8,7 +8,7 @@ export default function Cadastro(){
         const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-            redirectTo: `htpps://problemas.vercel.app/Home`,
+            redirectTo: `${window.location.origin}/Home`,
             queryParams: {
             access_type: 'offline',
             prompt: 'consent',
